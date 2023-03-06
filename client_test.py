e108fe44a9592a20d5bf7153e5b39a8e35773277
Роман Шафранюк <romanshafranuk@gmail.com>
2023-03-06 22:16:19 +0400

    Send messages

--- client_test.py
+++ client_test.py
@@ -1,8 +1,30 @@
 import socket
+import threading
+import sys
 
+buf = []
+def get_input():
+    global buf
+    while True:
+        buf.append(input('>>> '))
+
+port = int(sys.argv[1])
 sock = socket.socket()
-sock.connect(('localhost', 9090))
-sock.send(b'hello, world!')
+sock.connect(('localhost', port))
+msg = threading.Thread(target=get_input)
+msg.start()
+while True:
+    data = sock.recv(1024)
+    if data:
+        print(f"Recieve message: {data} ")
+    while len(buf) > 0:
+        print("Есть что отправить")
+        sock.send(buf.pop().encode("utf-8"))
+        print("Отправлено")
+    
+
+
+#sock.send(b'hello, world!')
 
 data = sock.recv(1024)
 sock.close()
--- server_test.py
+++ server_test.py
@@ -1,12 +1,22 @@
 import socket
+import threading
+import sys
+def connect(address, port):
+    sock = socket.socket()
+    sock.bind((address, port))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    print('connected:', conn, addr)
+    while True:
+        data = conn.recv(1024)
+        if len(data) > 0:
+            print(f"Receiving message: {data}")
+    #onn.close()
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
-print('connected:', conn, addr)
-conn1, addr1 = sock.accept()
-print('connected:', conn1, addr1)
 
-conn.close()
-conn1.close()
+client1 = threading.Thread(target=connect, args=('localhost', 9090))
+client2 = threading.Thread(target=connect, args=('localhost', 9091))
+client1.start()
+client2.start()
+while True:
+    pass
