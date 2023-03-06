036188f0fde94b24b08b11c32c917cdedfeba2bb
Роман Шафранюк <romanshafranuk@gmail.com>
2023-03-06 16:08:21 +0400

    Initial commit

--- server.py
+++ server.py
@@ -1,13 +1,70 @@
-from socket import *
+#!/usr/bin/env python3
+import sys
+import socket
+import selectors
+import types
 
-server = socket(AF_INET, SOCK_STREAM)
+sel  = selectors.DefaultSelector()
 
-server.bind(('192.168.1.34', 7000))
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", out=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
-server.listen(2)
 
-user, address = server.accept()
-print(f"CONNECTED:\n{user}\n{address}")
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
 
-user.send ("You are connected!".encode('utf-8'))
-#while True:
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
+
+
+
+
+
+
+
+
+
+#HOST = '192.168.1.34'  # Standard loopback interface address (localhost)
+#PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+
