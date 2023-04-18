bbc014496f9d3764c9a5939fa5c4a5dabbeb21a8
Divanchik <d-sh-2016@list.ru>
2023-04-18 12:13:06 +0400

    simple message protocol for sockets

---
+++ socketmessage.py
@@ -0,0 +1,24 @@
+# Simple message protocol for sockets.
+import socket
+
+
+def socket_send(sock: socket.socket, header: str, content: bytes):
+    # send header
+    header_data = header.encode('utf-8')
+    header_len = len(header_data)
+    sock.send(header_len.to_bytes(4, 'little'))
+    sock.send(header_data)
+    # send content
+    content_len = len(content)
+    sock.send(content_len.to_bytes(4, 'little'))
+    sock.send(content)
+
+
+def socket_recv(sock: socket.socket):
+    # recv header
+    header_len = int.from_bytes(sock.recv(4), 'little')
+    header = sock.recv(header_len).decode('utf-8')
+    # recv content
+    content_len = int.from_bytes(sock.recv(4), 'little')
+    content = sock.recv(content_len)
+    return header, content
